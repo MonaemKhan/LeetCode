5d1fece8076b78d006976c92ac5f8292e21b6cc3
M.A Monaem Khan <khanmonaem07@gmail.com>
2025-08-20 18:23:07 +0600

    update 20082025

---
+++ LeetCode/LetterCombinations.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Immutable;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(LetterCombinations(""));
+        Console.ReadKey();
+    }
+
+    public static IList<string> LetterCombinations(string digits)
+    {
+        Dictionary<char, List<string>> myDict = new Dictionary<char, List<string>>();
+        myDict['2'] = new List<string> { "a", "b" , "c" };
+        myDict['3'] = new List<string> { "d", "e" , "f"};
+        myDict['4'] = new List<string> { "g", "h" , "i"};
+        myDict['5'] = new List<string> { "j", "k" , "l"};
+        myDict['6'] = new List<string> { "m", "n" , "o"};
+        myDict['7'] = new List<string> { "p", "q" , "r" , "s"};
+        myDict['8'] = new List<string> { "t", "u" , "v"};
+        myDict['9'] = new List<string> { "w", "x" , "y" , "z"};
+
+        IList<string> temp = new List<string>();
+        IList<string> final = new List<string>();
+
+        var str = digits.ToArray();
+
+        foreach (var digit in str)
+        {
+            foreach (var obj in myDict[digit])
+            {
+                if (final.Count <= 0)
+                {
+                    temp.Add(obj);
+                }
+                else
+                {
+                    foreach (var item in final)
+                    {
+                        temp.Add(item + obj);
+                    }
+                }
+            }
+            final = temp;
+            temp = new List<string>();
+        }
+
+        return final;
+    }
+}
---
+++ LeetCode/ThreeSumClosest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Immutable;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine(ThreeSum([-1, 0, 1, 2, -1, -4]));
+        //Console.WriteLine(ThreeSum([-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0]));
+        //Console.WriteLine(ThreeSum([0, 1, 1]));
+        //Console.WriteLine(ThreeSum([0, 0, 0]));
+        Console.WriteLine(ThreeSumClosest([1, 1, 1, 0],-100));
+        Console.WriteLine(ThreeSumClosest([4, 0, 5, -5, 3, 3, 0, -4, -5],-2));
+        Console.ReadKey();
+    }
+
+    public static int ThreeSumClosest(int[] nums, int target)
+    {
+        int len = nums.Length;
+        Array.Sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = i + 1; j < len; j++)
+            {
+                for (int k = j + 1; k < len; k++)
+                {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == target)
+                    {
+                        closest = sum;
+                        break;
+                    }
+                    if (Math.Abs(sum - target) <= Math.Abs(closest - target) && Math.Abs(sum - target) != 0)
+                    {
+                        closest = sum;
+                    }
+                }
+            }
+        }
+        return closest;
+    }
+}
